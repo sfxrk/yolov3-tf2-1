828d87adfc2ba7c3881187621a1795b3a1c9a4ab
yingzwang <yingzwang@hotmail.com>
2020-02-24 22:37:29 +0100

    test setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='yolov3_tf2',
+      version='0.1',
+      url='https://github.com/yingzwang/yolov3-tf2',
+      author='Ying Wang',
+      author_email='yingzwang@hotmail.com',
+      packages=['yolov3_tf2'])
